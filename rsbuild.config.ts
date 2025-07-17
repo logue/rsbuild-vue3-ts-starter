954d8d7d8ad15fc86ae816d61710ce956b9f8402
logue <logue@hotmail.co.jp>
2025-07-17 20:50:51 +0900

    Initial commit.

---
+++ rsbuild.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from '@rsbuild/core';
+import { pluginVue } from '@rsbuild/plugin-vue';
+
+export default defineConfig({
+  plugins: [pluginVue()],
+  html: {
+    template: './index.html'
+  },
+  source: {
+    entry: {
+      index: './src/main.js'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+});
